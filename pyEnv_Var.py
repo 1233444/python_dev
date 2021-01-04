01653a746d18278f9fa3941bf2b619c031287503
1233444 <1233444@gmail.com>
2021-01-04 00:30:58 -0500

    minor update

--- pyEnv_Var.py
+++ pyEnv_Var.py
@@ -1,11 +1,11 @@
 import os
+import requests
 
-db_user = os.environ.get('DB_USER')
-db_password = os.environ.get('DB_PASS')
-
+db_user = os.environ.get("DB_USER")
+db_password = os.environ.get("DB_PASS")
 # assign sensitive information right inside the code, no good practice
 # db_password = 'my_db_pass_123!'
-
-
 print(db_user)
 print(db_password)
+r = requests.get("https://coreyms.com")
+print(r.status_code)
