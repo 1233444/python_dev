35561bcae5198246fe0726a777c1ab257aa92d2c
1233444 <1233444@gmail.com>
2021-01-04 00:51:20 -0500

    update minor

--- pyEnv_Var.py
+++ pyEnv_Var.py
@@ -1,6 +1,13 @@
+# Python Quick Tip: Hiding Passwords and Secret Keys in Environment Variables (Mac & Linux)
+# https://youtu.be/5iWhQWVXosU
+import math
 import os
+import sys
+
 import requests
 
+print(sys.version)
+print(sys.version_info)
 db_user = os.environ.get("DB_USER")
 db_password = os.environ.get("DB_PASS")
 # assign sensitive information right inside the code, no good practice
@@ -9,3 +16,7 @@ print(db_user)
 print(db_password)
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+
+name = input("Your Name?")
+print("Hello,", name)
+
